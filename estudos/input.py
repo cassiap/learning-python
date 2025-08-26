26e60401fbe8571b46c53b6f9dfa5f0bcdf4a7ed
Cássia Paixão <cassiiagabrielaa@hotmail.com>
2025-08-25 22:28:46 -0300

    Adicionei exercícios de if/elif

---
+++ estudos/input.py
@@ -0,0 +1,5 @@
+nome = input("Qual o seu nome? ")
+numero_1 = input("Digite um número: ")
+numero_2 = input("Digite outro número: ")
+print(f'O seu nome é {nome}')
+print(f'A soma dos números é {int(numero_1) + int(numero_2)}')  
